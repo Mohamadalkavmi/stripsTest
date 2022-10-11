485396d423c950f726143bd41c1dbaa602406de8
Mohamadalkavmi <mohamad.alkavmi@gmail.com>
2022-10-11 20:09:58 +0300

    add new post

---
+++ post/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+class Post(models.Model):
+  title =models.CharField(max_length =5000)
+  content =models.CharField(max_length =5000)
+  createted_at = models.DateTimeField(default = timezone.now())
+  image = models.ImageField(upload_to ='post-image/')
+
+  class Meta:
+    verbose_name =("Post")
+    verbose_name_plural =("Posts")
+
+  def __str__(self):
+    return self.name
+
+  def get_absoluter_url(self):
+    return revers("Post_detail" ,kwart ={"pk":self.pk} )
